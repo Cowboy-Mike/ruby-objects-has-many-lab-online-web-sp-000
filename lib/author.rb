e77dd72d44fc454d1d2fef31b1bbc0cad0ad1e00
Mike Nanto <mike@thecowboymechanic.com>
2021-02-12 07:04:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,30 @@
-class Author
-  attr_accessor :name
+class Author 
+  attr_accessor :name 
 
-  @@all = []
+@@all = []
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+def initialize(name)
+  @name = name 
+  @@all << name 
+end 
 
-  def self.all
-    @@all
-  end
+def self.all
+  @@all
+end 
 
-  def add_post(post)
-    post.author = self
-  end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
-    post.author = self
-  end
+def add_posts_by_title(title)
+  post = Post.new(title)
+  post.author = self 
+end 
 
-  def posts
-    Post.all.select {|post| post.author == self}
-  end
+def add_post(post)
+  post.author = self
+end 
 
-  def self.post_count
-    Post.all.count
-  end
-end
+def posts 
+  Posts.all.select {|post| post.author == self} 
+end 
+
+
+end 
