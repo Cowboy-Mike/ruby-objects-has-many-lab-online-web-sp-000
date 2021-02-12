a1fa2a245f784764d17fcab9a3bf12d333038119
Mike Nanto <mike@thecowboymechanic.com>
2021-02-12 06:54:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song 
-  attr_accessor :name, :author 
+  attr_accessor :name, :artist  
   
   @@all = []
   
@@ -13,6 +13,7 @@ class Song
   end 
   
   def artist_name
-    self.artist = 
+    artist.name if artist 
+  end 
 
 end 
